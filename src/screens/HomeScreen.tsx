fe3cf2c49dab0b2b4e5ce9839608704b1056ebf8
Douglas Denuzzi <97412953+karpaguy@users.noreply.github.com>
2023-07-10 21:27:22 -0300

    Alterações ae

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { error } from "console";
 import { useEffect, useState } from "react";
 import { convertToObject } from "typescript";
 
@@ -14,6 +15,7 @@ export default function HomeScreen(){
     const [salvaDescricao, setSalvaDescricao] = useState<string>();
     const [salvaId, setSalvaId] = useState<number>();
 
+    const [itemSelecionado, setItemSelecionado] = useState<Item>();
     const [optionNumber, setOptionNumber] = useState<number>();
 
     useEffect(function (){
@@ -22,7 +24,7 @@ export default function HomeScreen(){
             setItens(response.data);
         })
         .catch(function (error) {
-            alert(error);
+            alert(error);   
         });
     },[]);
 
@@ -41,35 +43,37 @@ export default function HomeScreen(){
         }
     }
 
-    function botaoSelecionarItem() {
-        if(salvaId !== undefined) {
-            axios.get(`http://localhost:4000/itens/${salvaId}`)
-            .then()
-            .catch();
-
-            location.reload()
-
+    function botaoSelecionarItem(id : number | undefined) {
+        if(id !== undefined) {
+            axios.get(`http://localhost:4000/api/itens/${id}`)
+            .then(function (response) {
+                setItemSelecionado(response.data);
+                // const itemSelecionado: Item = response.data;
+                console.log(itemSelecionado?.id);
+            })
+            .catch(function (error) {
+                alert(error);
+            });
         }
     }
 
-    function botaoEditarClicado() {
-        if ((salvaNome !== undefined) && (salvaDescricao !== undefined) && (salvaId !== undefined)) {
-            const itemToEdit: Item = {
-                nome: salvaNome,
-                descricao: salvaDescricao
-            }
+    // function botaoEditarClicado() {
+    //     if ((salvaNome !== undefined) && (salvaDescricao !== undefined) && (salvaId !== undefined)) {
+    //         const itemToEdit: Item = {
+    //             nome: salvaNome,
+    //             descricao: salvaDescricao
+    //         }
 
-            axios.post(`http://localhost:4000/api/itens/${salvaId}`, itemToEdit)
-            .then()
-            .catch();
+    //         axios.post(`http://localhost:4000/api/itens/${salvaId}`, itemToEdit)
+    //         .then()
+    //         .catch();
 
-            location.reload();
-    }}
+    //         location.reload();
+    // }}
 
-    function botaoDeletarClicado() {
-        if(salvaId !== undefined) {
-
-             axios.delete(`http://localhost:4000/api/itens/${salvaId}`)
+    function botaoDeletarClicado(id : number | undefined) {
+        if(id !== undefined) {
+             axios.delete(`http://localhost:4000/api/itens/${id}`)
              .then()
              .catch();
 
@@ -82,15 +86,15 @@ export default function HomeScreen(){
             <h1>Home</h1>
             <ul>
                 {itens.map(function(item){
-                    return <li>{item.id} → {item.nome} - {item.descricao}</li>
+                    return <li>{item.id} → {item.nome} - {item.descricao} <button onClick={function () {botaoSelecionarItem(item.id); setOptionNumber(2)}}>Selecionar</button></li>
                 })}
             </ul>
             <div>
                 <div>   
                     <button onClick={() => setOptionNumber(1)}>Criar</button>
-                    <button onClick={() => setOptionNumber(2)}>Editar</button>
-                    <button onClick={() => setOptionNumber(3)}>Deletar</button>
-                    <button onClick={() => setOptionNumber(4)}>Selecionar</button>
+                    {/* <button onClick={() => setOptionNumber(2)}>Editar</button> */}
+                    {/* <button onClick={() => setOptionNumber(3)}>Deletar</button> */}
+                    {/* <button onClick={() => setOptionNumber(2)}>Selecionar</button> */}
                 </div>
                 
                 {(optionNumber == 1) && (
@@ -108,6 +112,23 @@ export default function HomeScreen(){
 
                 {(optionNumber == 2) && (
                     <div>
+                        {/* <button onClick={alert(salvaId)!}>Selecionar</button>   */}
+                        <input value={itemSelecionado?.nome} onChange={function(e){
+                            setSalvaNome(e.target.value)
+                        }}></input>
+
+                        <input value={itemSelecionado?.descricao} onChange={function(e){
+                            setSalvaDescricao(e.target.value)
+                        }}></input>
+
+                        <button>Editar</button>
+                        <button onClick={() => botaoDeletarClicado(itemSelecionado?.id)}>Deletar</button>
+
+                    </div>
+                )}
+
+                {/* {(optionNumber == 3) && (
+                    <div>
                         <input placeholder="Id" onChange={function(e){
                             setSalvaId(+e.target.value);
                         }} /> 
@@ -122,7 +143,7 @@ export default function HomeScreen(){
                     </div>
                 )}  
 
-                {(optionNumber == 3) && (
+                {(optionNumber == 4) && (
                     <div>
                         <input placeholder="Id" onChange={function(e){
                             setSalvaId(+e.target.value);
@@ -130,18 +151,7 @@ export default function HomeScreen(){
 
                     <button onClick={botaoDeletarClicado}>Deletar</button>   
                     </div>
-                )}
-
-                {(optionNumber == 4) && (
-                    <div>
-                        <input placeholder="Id" onChange={function(e){
-                            setSalvaId(+e.target.value);
-                        }} /> 
-
-                    <button onClick={botaoSelecionarItem}>Selecionar</button>   
-                    </div>
-                )}
-
+                )} */}
             </div>
         </div>
     )
